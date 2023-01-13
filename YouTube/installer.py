e1c6bf9bb769c3d4bd1e47185823f8393f0abcb0
MOHAMED19OS <mohamed_os@gmail.com>
2023-01-13 19:16:57 +0100

    Update Script Install Youtube

--- YouTube/installer.py
+++ YouTube/installer.py
@@ -2,10 +2,11 @@
 # code: BY MOHAMED_OS
 
 
-from os import popen, system
-from os.path import isfile
+from os import chdir, popen, remove, system
+from os.path import isfile, join
 from re import MULTILINE, findall
 from sys import version_info
+from time import sleep
 
 if version_info[0] == 3:
     from urllib.error import HTTPError, URLError
@@ -42,12 +43,14 @@ class YouTube():
             self.status = '/var/lib/opkg/status'
             self.update = 'opkg update >/dev/null 2>&1'
             self.install = 'opkg install'
+            self.uninstall = 'opkg remove --force-depends'
             self.list = 'opkg list-installed'
             self.extension = 'ipk'
         else:
             self.status = '/var/lib/dpkg/status'
             self.update = 'apt-get update >/dev/null 2>&1'
             self.install = 'apt-get install'
+            self.uninstall = 'apt-get purge --auto-remove'
             self.list = 'apt-get list-installed'
             self.extension = 'deb'
         return isfile('/etc/opkg/opkg.conf')
@@ -73,12 +76,13 @@ class YouTube():
         except URLError as e:
             print('URL Error: ', e.reason)
 
-    def check(self):
-        return popen(" ".join([self.list, self.package])).read().split("-")[-2].strip()
+    def version(self):
+        check = popen(" ".join([self.list, self.package])).read().split("-")[-2].strip()
+        return check.split('+')[-2]
 
     def banner(self):
         system('clear')
-        print(B,r"""
+        print(B, r"""
 Y88b   d88P            88888888888       888
  Y88b d88P                 888           888
   Y88o88P                  888           888
@@ -88,7 +92,71 @@ Y88b   d88P            88888888888       888
     888  Y88..88P Y88b 888 888  Y88b 888 888 d88P Y8b.
     888   "Y88P"   "Y88888 888   "Y88888 88888P"   "Y8888""", C)
 
+    def main(self):
+        self.stb_Image()
+
+        self.banner()
+        print("\n{}(!){} Please Wait Check Package ...".format(R, C))
+        system(self.update)
+        sleep(1)
+
+        if not self.stb_Image():
+            self.depends.extend("gstreamer1.0-plugins-base-meta",
+                                "gstreamer1.0-plugins-good-spectrum")
+
+        for pkg_name in self.depends:
+            if not self.package_check(pkg_name):
+                system('clear')
+                print("     Need To InsTall : {}{}{}\n".format(Y, pkg_name, C))
+                system(" ".join([self.install, pkg_name]))
+                sleep(1)
+
+        system('clear')
+        self.banner()
+        sleep(2)
+
+        file = self.info()
+
+        if isfile(join('/tmp/', file)):
+            remove(join('/tmp/', file))
+            sleep(0.8)
+
+        if self.version() == file.split('+')[-2].strip():
+            print('\nyou are use the latest version: {}{}{}\n'.format(
+                Y, file.split('+')[-2].strip(), C).capitalize())
+            sleep(0.8)
+            print("   Written by {}MOHAMED_OS{} (͡๏̯͡๏)\n".format(R, C))
+            exit()
+        else:
+            system("".join([self.uninstall, file.split('_')[0]]))
+
+        system('clear')
+        self.banner()
+        print("\n{}Please Wait{} while we Download And Install {}YouTube{} ...".format(
+            G, C, Y, C))
+
+        chdir('/tmp')
+
+        urlretrieve("".join([self.link, file]), filename=file)
+        sleep(0.8)
+
+        system(" ".join([self.install, file]))
+        sleep(1)
+
+        if isfile(join('/tmp/', file)):
+            remove(join('/tmp/', file))
+            sleep(0.8)
+
+        if isfile('/etc/enigma2/YouTube.key-opkg'):
+            remove('/etc/enigma2/YouTube.key-opkg')
+
+        if self.stb_Image():
+            system('killall -9 enigma2')
+        else:
+            system('systemctl restart enigma2')
+
+
 if __name__ == '__main__':
     build = YouTube()
-    build.banner()
+    build.main()
     print("   Written by {}MOHAMED_OS{} (͡๏̯͡๏)".format(R, C))
