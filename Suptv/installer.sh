21638ec6dd48772fee6a1ead523c8357bddb6b19
MOHAMED_OS <mohamed_os@gmail.com>
2023-05-13 07:42:18 +0100

    Update IPK: version 4.1 For All Python

--- Suptv/installer.sh
+++ Suptv/installer.sh
@@ -10,9 +10,8 @@
 ###########################################
 # Configure where we can find things here #
 TMPDIR='/tmp'
-VERSION='4.3.1-r0'
+VERSION='4.1'
 MY_URL='https://raw.githubusercontent.com/MOHAMED19OS/Download/main/Suptv'
-PYTHON_VERSION=$(python -c"import sys; print(sys.version_info.major)")
 
 ####################
 #  Image Checking  #
@@ -29,44 +28,32 @@ elif [ -f /etc/apt/apt.conf ]; then
     DPKINSTALL='dpkg -i --force-overwrite'
 fi
 
-if [ "$PYTHON_VERSION" -eq 3 ]; then
-    echo ":You have Python3 image ..."
-    PACKAGE='enigma2-plugin-extensions-suptv-python3'
-else
-    echo ":You have Python2 image ..."
-    if [ $OSTYPE = "Opensource" ]; then
-        PACKAGE='enigma2-plugin-extensions-suptv-python2'
-    elif [ $OSTYPE = "DreamOS" ]; then
-        VERSION='3.8'
-        PACKAGE='enigma2-plugin-extensions-suptv'
-    fi
-fi
-
 ##################################
 # Remove previous files (if any) #
 rm -rf $TMPDIR/"${PACKAGE:?}"* >/dev/null 2>&1
 
-if [ "$($OPKGLIST $PACKAGE | awk '{ print $3 }')" = $VERSION ]; then
+if [ "$($OPKGLIST "$PACKAGE" | awk '{ print $3 }')" = $VERSION ]; then
     echo " You are use the laste Version: $VERSION"
     exit 1
-elif [ -z "$($OPKGLIST $PACKAGE | awk '{ print $3 }')" ]; then
+elif [ -z "$($OPKGLIST "$PACKAGE" | awk '{ print $3 }')" ]; then
     echo
     clear
 else
-    $OPKGREMOV $PACKAGE
+    $OPKGREMOV "$PACKAGE"
 fi
+
 $OPKG >/dev/null 2>&1
 ###################
 #  Install Plugin #
 
 echo "Insallling Suptv plugin Please Wait ......"
 
-if [ $OSTYPE = "Opensource" ]; then
-    wget $MY_URL/${PACKAGE}_${VERSION}_all.ipk -qP $TMPDIR
-    $OPKGINSTAL $TMPDIR/${PACKAGE}_${VERSION}_all.ipk
-elif [ $OSTYPE = "DreamOS" ]; then
-    wget $MY_URL/${PACKAGE}_${VERSION}_all.deb -qP $TMPDIR
-    $DPKINSTALL $TMPDIR/${PACKAGE}_${VERSION}.deb
+if [ "$OSTYPE" = "Opensource" ]; then
+    wget $MY_URL/"${PACKAGE}"_${VERSION}_all.ipk -qP $TMPDIR
+    $OPKGINSTAL $TMPDIR/"${PACKAGE}"_${VERSION}_all.ipk
+elif [ "$OSTYPE" = "DreamOS" ]; then
+    wget $MY_URL/"${PACKAGE}"_${VERSION}_all.deb -qP $TMPDIR
+    $DPKINSTALL $TMPDIR/"${PACKAGE}"_${VERSION}.deb
     $OPKGINSTAL -f -y
 
 fi
