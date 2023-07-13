9f50d74d7083fba5397daec8b50dd38e328c9f3f
soomarali <40350718+soomarali@users.noreply.github.com>
2023-07-13 11:13:02 +0500

    Update installer.sh

--- SmartCam/installer.sh
+++ SmartCam/installer.sh
@@ -1,1 +1,87 @@
+#!/bin/sh
+# ###########################################
+# SCRIPT : DOWNLOAD AND INSTALL XcPlugin Forever
+# ###########################################
+#
+# Command: wget https://raw.githubusercontent.com/MOHAMED19OS/Download/main/XC-Code/installer.sh -qO - | /bin/sh
+#
+# ###########################################
+
+###########################################
+# Configure where we can find things here #
+TMPDIR='/tmp'
+PACKAGE='enigma2-plugin-SmartCam'
+MY_URL='https://patbuweb.com/xcplugin'
+URL_VER=''
+
+#################
+# Check Version #
+VERSION=$(wget $URL_VER/version -qO- | cut -d "=" -f2-)
+
+####################
+#  Image Checking  #
+
+if [ -f /etc/opkg/opkg.conf ]; then
+    OSTYPE='Opensource'
+    OPKGINSTAL='opkg install'
+    OPKGLIST='opkg list-installed'
+    OPKGREMOV='opkg remove --force-depends'
+elif [ -f /etc/apt/apt.conf ]; then
+    OSTYPE='DreamOS'
+    OPKGINSTAL='apt-get install'
+    OPKGLIST='apt-get list-installed'
+    OPKGREMOV='apt-get purge --auto-remove'
+    DPKINSTALL='dpkg -i --force-overwrite'
+fi
+
+##################################
+# Remove previous files (if any) #
+rm -rf $TMPDIR/"${PACKAGE:?}"* >/dev/null 2>&1
+
+######################
+#  Remove Old Plugin #
+if [ "$($OPKGLIST $PACKAGE | awk '{ print $3 }')" = "$VERSION" ]; then
+    echo " You are use the laste Version: $VERSION"
+    exit 1
+elif [ -z "$($OPKGLIST $PACKAGE | awk '{ print $3 }')" ]; then
+    echo
+    clear
+else
+    $OPKGREMOV $PACKAGE
+fi
+
+###################
+#  Install Plugin #
+echo "SmartCam plugin Please Wait ......"
+if [ "$OSTYPE" = "Opensource" ]; then
+    wget $MY_URL/${PACKAGE}_"${VERSION}"_all.ipk -qP $TMPDIR
+    $OPKGINSTAL $TMPDIR/${PACKAGE}_"${VERSION}"_all.ipk
+else
+    wget $MY_URL/${PACKAGE}_"${VERSION}"_all.deb -qP $TMPDIR
+    $DPKINSTALL $TMPDIR/${PACKAGE}_"${VERSION}"_all.deb
+    $OPKGINSTAL -f -y
+fi
+
+#########################
+# Remove files (if any) #
+rm -rf $TMPDIR/"${PACKAGE:?}"*
+
+sleep 1
+clear
+echo ""
+echo "****************************************************************************************"
+echo "**                                                                                     *"
+echo "**                            XcPlugin   : $VERSION                                    *"
+echo "**                            Uploaded by: Asghar_Ai                                   *"
+echo "**                                                                                     *"
+echo "****************************************************************************************"
+echo ""
+
+if [ "$OSTYPE" = "Opensource" ]; then
+    killall -9 enigma2
+else
+    systemctl restart enigma2
+fi
+
+exit 0
 
