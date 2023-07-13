8306fbfca2cbb11b02a527a45b37d1b3b1fe816d
soomarali <40350718+soomarali@users.noreply.github.com>
2023-07-13 20:23:11 +0500

    Update installer.sh

--- SmartCam/installer.sh
+++ SmartCam/installer.sh
@@ -15,3 +15,12 @@ if [ $? -eq 0 ]; then
 else
   echo "File download failed."
 fi
+# Install the IPK file
+opkg install "enigma2-smartcam_11.572_all.ipk"
+
+# Check the installation status
+if [ $? -eq 0 ]; then
+  echo "Package installed successfully."
+else
+  echo "Package installation failed."
+fi
