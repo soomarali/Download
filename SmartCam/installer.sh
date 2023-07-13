4937edbf3164fb561d061a421ffcbf2db22d531d
soomarali <40350718+soomarali@users.noreply.github.com>
2023-07-13 18:42:34 +0500

    Update installer.sh

--- SmartCam/installer.sh
+++ SmartCam/installer.sh
@@ -1,69 +1,96 @@
 #!/bin/sh
-# ###########################################
-# SCRIPT : DOWNLOAD AND INSTALL SmartCam
-# ###########################################
-#
-# Command: wget https://raw.githubusercontent.com/soomarali/Download/main/SmartCam/installer.sh -qO - | /bin/sh
-#
-# ###########################################
 
-###########################################
-# Configure where we can find things here #
-TMPDIR='/tmp'
-PACKAGE='enigma2-smartcam'
-MY_URL='https://raw.githubusercontent.com/soomarali/Download/main/SmartCam'
-URL_VER=''
+# ==============================================
+# SCRIPT : DOWNLOAD AND INSTALL OSCamSmartCam #
+# =====================================================================================================================
+# Command: wget https://raw.githubusercontent.com/soomarali/Download/main/SmartCam/installer.sh -O - | /bin/sh #
+# =====================================================================================================================
 
-#################
-# Check Version #
-VERSION=$(wget $URL_VER/version -qO- | cut -d "=" -f2-)
+########################################################################################################################
+# Plugin	... Enter Manually
+########################################################################################################################
 
-####################
-#  Image Checking  #
+PACKAGE_DIR='Download/main/SmartCam'
 
-if [ -f /etc/opkg/opkg.conf ]; then
-    OSTYPE='Opensource'
-    OPKGINSTAL='opkg install'
-    OPKGLIST='opkg list-installed'
-    OPKGREMOV='opkg remove --force-depends'
-elif [ -f /etc/apt/apt.conf ]; then
-    OSTYPE='DreamOS'
-    OPKGINSTAL='apt-get install'
-    OPKGLIST='apt-get list-installed'
-    OPKGREMOV='apt-get purge --auto-remove'
-    DPKINSTALL='dpkg -i --force-overwrite'
+MY_IPK="enigma2-smartcam_11.572_all.ipk"
+MY_DEB="enigma2-smartcam_11.572_all.deb"
 
-#  Install Plugin #
-echo "SmartCam server plugin Please Wait ......"
-if [ "$OSTYPE" = "Opensource" ]; then
-    wget $MY_URL/${PACKAGE}_"${VERSION}"_all.ipk -qP $TMPDIR
-    $OPKGINSTAL $TMPDIR/${PACKAGE}_"${VERSION}"_all.ipk
+########################################################################################################################
+# Auto ... Do not change
+########################################################################################################################
+
+# Decide : which package ?
+MY_MAIN_URL="https://raw.githubusercontent.com/soomarali/Download/main"
+if which dpkg > /dev/null 2>&1; then
+	MY_FILE=$MY_DEB
+	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_DEB
 else
-    wget $MY_URL/${PACKAGE}_"${VERSION}"_all.deb -qP $TMPDIR
-    $DPKINSTALL $TMPDIR/${PACKAGE}_"${VERSION}"_all.deb
-    $OPKGINSTAL -f -y
+	MY_FILE=$MY_IPK
+	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_IPK
+fi
+MY_TMP_FILE="/tmp/"$MY_FILE
+
+echo ''
+echo '************************************************************'
+echo '**                         STARTED                        **'
+echo '************************************************************'
+echo "**                 Uploaded by: ASGHAR_ALI                **"
+echo "**  https://www.tunisia-sat.com/forums/threads/3942965/   **"
+echo "************************************************************"
+echo ''
+
+# Remove previous file (if any)
+rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+# Download package file
+MY_SEP='============================================================='
+echo $MY_SEP
+echo 'Downloading '$MY_FILE' ...'
+echo $MY_SEP
+echo ''
+wget -T 2 $MY_URL -P "/tmp/"
+
+# Check download
+if [ -f $MY_TMP_FILE ]; then
+	# Install
+	echo ''
+	echo $MY_SEP
+	echo 'Installation started'
+	echo $MY_SEP
+	echo ''
+	if which dpkg > /dev/null 2>&1; then
+		dpkg -i --force-overwrite $MY_TMP_FILE
+		apt install -f -y
+	else
+		opkg install --force-reinstall $MY_TMP_FILE	
+	fi
+	MY_RESULT=$?
+
+	# Result
+	echo ''
+	echo ''
+	if [ $MY_RESULT -eq 0 ]; then
+		echo "   >>>>   SUCCESSFULLY INSTALLED   <<<<"
+		echo ''
+		echo "   >>>>         RESTARING         <<<<"
+		if which systemctl > /dev/null 2>&1; then
+			sleep 2; systemctl restart enigma2
+		else
+			init 4; sleep 4; init 3;
+		fi
+	else
+		echo "   >>>>   INSTALLATION FAILED !   <<<<"
+	fi;
+	echo ''
+	echo '**************************************************'
+	echo '**                   FINISHED                   **'
+	echo '**************************************************'
+	echo ''
+	exit 0
+else
+	echo ''
+	echo "Download failed !"
+	exit 1
 fi
 
-#########################
-# Remove files (if any) #
-rm -rf $TMPDIR/"${PACKAGE:?}"*
-
-sleep 1
-clear
-echo ""
-echo "****************************************************************************************"
-echo "**                                                                                     *"
-echo "**                            SmartCam   : $VERSION                                    *"
-echo "**                            Uploaded by: Asghar_Ai                                   *"
-echo "**                                                                                     *"
-echo "****************************************************************************************"
-echo ""
-
-if [ "$OSTYPE" = "Opensource" ]; then
-    killall -9 enigma2
-else
-    systemctl restart enigma2
-fi
-
-exit 0
-
+# ------------------------------------------------------------------------------------------------------------
