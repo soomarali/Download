8b16f5395c7934c2c6945714eaf0ede221bb298b
soomarali <40350718+soomarali@users.noreply.github.com>
2023-07-13 20:15:33 +0500

    Update installer.sh

--- SmartCam/installer.sh
+++ SmartCam/installer.sh
@@ -1,78 +1,17 @@
 #!/bin/sh
 
-# ==============================================
-# SCRIPT : DOWNLOAD AND INSTALL  #SmartCam
-# =====================================================================================================================
-# Command: wget https://raw.githubusercontent.com/biko-73/EvgQuickSignal/main/installer.sh -O - | /bin/sh #
-# =====================================================================================================================
+# URL of the file to download
+FILE_URL="https://raw.githubusercontent.com/soomarali/Download/main/SmartCam/enigma2-smartcam_11.572_all.ipk"
 
-PACKAGE_DIR='Download/main/SmartCam'
-MY_IPK="enigma2-plugin-extensions-evgquicksignal_1.2_all.ipk"
+# Destination path to save the downloaded file
+DESTINATION="/tmp"
 
-MY_MAIN_URL="https://raw.githubusercontent.com/soomarali/enigma2-plugin-extensions-evgquicksignal_1.2_all.ipk"
-if which opkg > /dev/null 2>&1; then
-	MY_FILE=$MY_IPK
-	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_IPK
+# Download the file using wget
+wget -O "$enigma2-smartcam_11.572_all.ipk" "$https://raw.githubusercontent.com/soomarali/Download/main/SmartCam/enigma2-smartcam_11.572_all.ipk"
+
+# Check the download status
+if [ $? -eq 0 ]; then
+  echo "File downloaded successfully."
 else
-	echo
-	echo ======================================
-	echo == OPKG not found. Cannot continue. ==
-	echo ======================================
-	echo
-	exit 1
+  echo "File download failed."
 fi
-MY_TMP_FILE="/tmp/"$MY_FILE
-
-echo ''
-echo '****************************************************************************'
-echo '**                                 STARTED                                **'
-echo '****************************************************************************'
-echo "**                            Uploaded by: Biko_73                        **"
-echo "**  https://www.tunisia-sat.com/forums/threads/4264626/#post-1055273465   **"
-echo "****************************************************************************"
-echo ''
-
-rm -f $MY_TMP_FILE > /dev/null 2>&1
-
-MY_SEP='============================================================='
-echo $MY_SEP
-echo 'Downloading '$MY_FILE' ...'
-echo $MY_SEP
-echo ''
-
-wget -T 2 $MY_URL -P "/tmp/"
-
-if [ -f $MY_TMP_FILE ]; then
-	# Install
-	echo ''
-	echo $MY_SEP
-	echo 'Installation started'
-	echo $MY_SEP
-	echo ''
-
-	opkg install --force-reinstall $MY_TMP_FILE
-	MY_RESULT=$?
-
-	echo ''
-	echo ''
-	if [ $MY_RESULT -eq 0 ]; then
-		echo "   >>>>   SUCCESSFULLY INSTALLED   <<<<"
-		echo ''
-		echo "   >>>>         RESTARING         <<<<"
-		init 4; sleep 4; init 3;
-	else
-		echo "   >>>>   INSTALLATION FAILED !   <<<<"
-	fi;
-	echo ''
-	echo '**************************************************'
-	echo '**                   FINISHED                   **'
-	echo '**************************************************'
-	echo ''
-	exit 0
-else
-	echo ''
-	echo "Download failed !"
-	exit 1
-fi
-
-# ------------------------------------------------------------------------------------------------------------
