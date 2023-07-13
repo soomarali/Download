a7b517ef7024df55c262e6d632ce15872a8b6875
soomarali <40350718+soomarali@users.noreply.github.com>
2023-07-13 20:18:22 +0500

    Update installer.sh

--- SmartCam/installer.sh
+++ SmartCam/installer.sh
@@ -4,10 +4,10 @@
 FILE_URL="https://raw.githubusercontent.com/soomarali/Download/main/SmartCam/enigma2-smartcam_11.572_all.ipk"
 
 # Destination path to save the downloaded file
-DESTINATION="/tmp"
+DESTINATION="/var/tmp/"
 
 # Download the file using wget
-wget -O "$enigma2-smartcam_11.572_all.ipk" "$https://raw.githubusercontent.com/soomarali/Download/main/SmartCam/enigma2-smartcam_11.572_all.ipk"
+wget -O "enigma2-smartcam_11.572_all.ipk" "https://raw.githubusercontent.com/soomarali/Download/main/SmartCam/enigma2-smartcam_11.572_all.ipk"
 
 # Check the download status
 if [ $? -eq 0 ]; then
