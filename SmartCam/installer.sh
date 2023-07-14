aaf35ecc1663b3a6d4fc802487f2264e607d37cf
soomarali <40350718+soomarali@users.noreply.github.com>
2023-07-14 19:56:19 +0500

    Update installer.sh

--- SmartCam/installer.sh
+++ SmartCam/installer.sh
@@ -1,46 +1,100 @@
 #!/bin/sh
 
-# ###########################################
-# SCRIPT : DOWNLOAD AND INSTALL OSCAM SMARTCAM
-# ###########################################
-#
-# Command: wget https://raw.githubusercontent.com/soomarali/Download/main/SmartCam/installer.sh -qO - | /bin/sh
-#
-# ###########################################
-# URL of the file to download
-FILE_URL="https://raw.githubusercontent.com/soomarali/Download/main/SmartCam/enigma2-smartcam_11546_all.ipk"
+# ==============================================
+# SCRIPT : DOWNLOAD AND INSTALL OSCam SmartCam #
+# =====================================================================================================================
+# Command: wget https://raw.githubusercontent.com/soomarali/Download/main/SmartCam/installer.sh -qO - | /bin/sh #
+# =====================================================================================================================
 
-# Destination path to save the downloaded file
-DESTINATION="/var/tmp/"
+########################################################################################################################
+# Plugin	... Enter Manually
+########################################################################################################################
 
-# Download the file using wget
-wget -O "/var/tmp/enigma2-smartcam_11546_all.ipk" "https://raw.githubusercontent.com/soomarali/Download/main/SmartCam/enigma2-smartcam_11546_all.ipk"
+PACKAGE_DIR='Download//main/SmartCam'
 
-opkg install /var/tmp/enigma2-smartcam_11546_all.ipk
-# Check the download status
-if [ $? -eq 0 ]; then
-  echo "File downloaded successfully."
+MY_IPK="enigma2-smartcam_11546_all.ipk"
+MY_DEB="enigma2-smartcam_11.572_all.deb"
+
+########################################################################################################################
+# Auto ... Do not change
+########################################################################################################################
+
+# Decide : which package ?
+MY_MAIN_URL="https://raw.githubusercontent.com/soomarali/"
+if which dpkg > /dev/null 2>&1; then
+	MY_FILE=$MY_DEB
+	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_DEB
 else
-  echo "File download failed."
+	MY_FILE=$MY_IPK
+	MY_URL=$MY_MAIN_URL$PACKAGE_DIR'/'$MY_IPK
 fi
-# Install the IPK file
-#opkg install "enigma2-smartcam_11.572_all.ipk"
+MY_TMP_FILE="/tmp/"$MY_FILE
 
-# Check the installation status
-if [ $? -eq 0 ]; then
-  echo "Package installed successfully."
+echo ''
+echo '************************************************************'
+echo '**                         STARTED                        **'
+echo '************************************************************'
+echo "**                 Uploaded by : ASGHAR ALI                **"
+echo "**                 Devolped For: DREAMWORLD                **"
+echo "**                 SUPPORT     : 03357300604               **"
+echo "************************************************************"
+echo ''
+
+# Remove previous file (if any)
+rm -f $MY_TMP_FILE > /dev/null 2>&1
+
+# Download package file
+MY_SEP='============================================================='
+echo $MY_SEP
+echo 'Downloading '$MY_FILE' ...'
+echo $MY_SEP
+echo ''
+wget -T 2 $MY_URL -P "/tmp/"
+
+# Check download
+if [ -f $MY_TMP_FILE ]; then
+	# Install
+	echo ''
+	echo $MY_SEP
+	echo 'Installation started'
+	echo $MY_SEP
+	echo ''
+	if which dpkg > /dev/null 2>&1; then
+		dpkg -i --force-overwrite $MY_TMP_FILE
+		apt install -f -y
+	else
+		opkg install --force-reinstall $MY_TMP_FILE	
+	fi
+	MY_RESULT=$?
+
+	# Result
+	echo ''
+	echo ''
+	if [ $MY_RESULT -eq 0 ]; then
+		echo "   >>>>   SUCCESSFULLY INSTALLED   <<<<"
+		echo ''
+		echo "   >>>>         RESTARING         <<<<"
+		if which systemctl > /dev/null 2>&1; then
+			sleep 2; systemctl restart enigma2
+		else
+			init 4; sleep 4; init 3;
+		fi
+	else
+		echo "   >>>>   INSTALLATION FAILED !   <<<<"
+	fi;
+	echo ''
+	echo '**************************************************'
+	echo '**                   FINISHED                   **'
+	echo '**	Uploaded BY : ASGHAR ALI	      **'
+ 	echo '**	Devolped For: DREAMWORLD 	      **'
+  	echo '**	SUPPORT     : 03357300604	      **'  
+ 	echo '**************************************************'
+	echo ''
+	exit 0
 else
-  echo "Package installation failed."
+	echo ''
+	echo "Download failed !"
+	exit 1
 fi
-echo ""
-echo "***********************************************************************"
-echo "**                                                                    *"
-echo "**                       SmartCam     : v4                            *"
-echo "**                       Uploaded by: ASGHAR_ALI                      *"
-echo "**                       Develop For : DREAMWORLD                     *"
-echo "**                       Support     : 03357300604                    *"
-echo "**                                                                    *"
-echo "***********************************************************************"
-echo ""
 
-exit 0
+# ------------------------------------------------------------------------------------------------------------
